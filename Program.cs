baabdc5b089727fb7740ca1e8714da06028635f1
gabriel-slovak <slovakgabriellopes@gmail.com>
2021-03-13 17:03:37 -0300

    finalizacão do programa Leet

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string traduzir; 
+
+            Console.WriteLine("-------------");
+            Console.WriteLine("Tradutor leet");
+            Console.WriteLine("-------------");
+            
+            Console.WriteLine();
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("Insira seu texto para transformalo em Leet: ");
+            Console.ResetColor();
+            traduzir = Console.ReadLine();
+            Console.Write("\n");
+            Console.WriteLine();
+
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.Write("TRADUZIDO: ");
+            Console.ResetColor();;
+            Console.WriteLine(traduzir
+            .Replace("A", "4")
+            .Replace("a", "4")
+            .Replace("E", "3")
+            .Replace("e", "3")
+            .Replace("I", "1")
+            .Replace("i", "1")
+            .Replace("L","1")
+            .Replace("l", "1")
+            .Replace("O", "0")
+            .Replace("o", "0")
+            .Replace("T", "7")
+            .Replace("t", "7")
+            .Replace("S", "5")
+            .Replace("s", "5"));
+            Console.WriteLine();
+
+        
+        }
+    }
+}
